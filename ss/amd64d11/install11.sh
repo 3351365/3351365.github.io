abfe2d4ee8912907999d9296794ea45bb6a2c9bb
3351365 <89839879+3351365@users.noreply.github.com>
2022-09-20 17:54:11 +0800

    Update install11.sh

--- ss/amd64d11/install11.sh
+++ ss/amd64d11/install11.sh
@@ -16,7 +16,7 @@ cd /tmp
 apt-get -q update
 apt-get install -y --no-install-recommends -f -qq apg asciidoc debhelper po-debconf intltool-debian libev-dev libpcre3-dev pkg-config xmlto libcap2-bin libpam-cap
 
-DebList="libc-bin_2.31-13+deb11u4_$Bit.deb libc6_2.31-13+deb11u4_$Bit.deb locales-all_2.31-13+deb11u4_$Bit.deb libc-dev-bin_2.31-13+deb11u4_$Bit.deb libc6-dev_2.31-13+deb11u4_$Bit.deb libudns0_0.4-1+b1_$Bit.deb libc-ares2_1.17.1-1+deb11u1_$Bit.deb libc-ares-dev_1.17.1-1+deb11u1_$Bit.deb libsodium23_1.0.18-1_$Bit.deb libsodium-dev_1.0.18-1_$Bit.deb libcork16_0.15.0+ds-16_$Bit.deb libcork-dev_0.15.0+ds-16_$Bit.deb libcorkipset1_1.1.1+20150311-11_$Bit.deb ibcorkipset-dev_1.1.1+20150311-11_$Bit.deb libbloom1_1.6-3_$Bit.deb libbloom-dev_1.6-3_$Bit.deb libmbedcrypto3_2.16.9-0.1_$Bit.deb libmbedx509-0_2.16.9-0.1_$Bit.deb libmbedtls12_2.16.9-0.1_$Bit.deb libmbedtls-dev_2.16.9-0.1_$Bit.deb shadowsocks-libev_3.3.5+ds-4_$Bit.deb simple-obfs_0.0.5-6_$Bit.deb"
+DebList="libc-bin_2.31-13+deb11u4_$Bit.deb libc6_2.31-13+deb11u4_$Bit.deb locales-all_2.31-13+deb11u4_$Bit.deb libc-dev-bin_2.31-13+deb11u4_$Bit.deb libc6-dev_2.31-13+deb11u4_$Bit.deb libudns0_0.4-1+b1_$Bit.deb libc-ares2_1.17.1-1+deb11u1_$Bit.deb libc-ares-dev_1.17.1-1+deb11u1_$Bit.deb libsodium23_1.0.18-1_$Bit.deb libsodium-dev_1.0.18-1_$Bit.deb libcork16_0.15.0+ds-16_$Bit.deb libcork-dev_0.15.0+ds-16_$Bit.deb libcorkipset1_1.1.1+20150311-11_$Bit.deb libcorkipset-dev_1.1.1+20150311-11_$Bit.deb libbloom1_1.6-3_$Bit.deb libbloom-dev_1.6-3_$Bit.deb libmbedcrypto3_2.16.9-0.1_$Bit.deb libmbedx509-0_2.16.9-0.1_$Bit.deb libmbedtls12_2.16.9-0.1_$Bit.deb libmbedtls-dev_2.16.9-0.1_$Bit.deb shadowsocks-libev_3.3.5+ds-4_$Bit.deb simple-obfs_0.0.5-6_$Bit.deb"
 
 for DebINS in `echo $DebList`
 do
