a104b5fafaa6f8a321500dac5b6bf7e700e2e4b9
3351365 <89839879+3351365@users.noreply.github.com>
2022-09-22 12:49:09 +0800

    Update install11v2ray-plugin.sh

--- ss/amd64d11/install11v2ray-plugin.sh
+++ ss/amd64d11/install11v2ray-plugin.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 arch="$(dpkg --print-architecture)"
-[ -n "$arch" ] || exit 1
-[ "$arch" == 'amd64' -o "$arch" == 'i386' ] || exit 1
 wget --no-check-certificate -qO "/tmp/shadowsocks-v2ray-plugin_1.3.1-3+b6_amd64.deb" "https://3351365.github.io/ss/amd64d11/shadowsocks-v2ray-plugin_1.3.1-3+b6_amd64.deb"
 dpkg -i "/tmp/shadowsocks-v2ray-plugin_1.3.1-3+b6_amd64.deb"
