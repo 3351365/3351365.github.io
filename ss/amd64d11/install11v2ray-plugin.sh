58abc547d9c13e7cc3efcef5d24b6b8f59e7872a
3351365 <89839879+3351365@users.noreply.github.com>
2022-09-22 13:00:39 +0800

    Update install11v2ray-plugin.sh

--- ss/amd64d11/install11v2ray-plugin.sh
+++ ss/amd64d11/install11v2ray-plugin.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
+DataURL='https://3351365.github.io/ss'
+kBit=$(getconf LONG_BIT)
+[ "$kBit" == '32' ] && Bit='i386'
+[ "$kBit" == '64' ] && Bit='amd64'
+
 mkdir -p /tmp
 cd /tmp
-arch="$(dpkg --print-architecture)"
-wget --no-check-certificate -qO "/tmp/shadowsocks-v2ray-plugin_1.3.1-3+b6_amd64.deb" "https://3351365.github.io/ss/amd64d11/shadowsocks-v2ray-plugin_1.3.1-3+b6_amd64.deb"
-dpkg -i "/tmp/shadowsocks-v2ray-plugin_1.3.1-3+b6_amd64.deb"
+
+apt-get -q update
+apt-get install -y --no-install-recommends -f -qq apg asciidoc debhelper po-debconf intltool-debian libev-dev libpcre3-dev pkg-config xmlto libcap2-bin libpam-cap
+
+DebList="shadowsocks-v2ray-plugin_1.3.1-3+b6_$Bit.deb"
+
+for DebINS in `echo $DebList`
+do
+  echo -ne '\033[33m'${DebINS}'\033[0m\t'
+  [ -f ''${DebINS}'' ] || wget --no-check-certificate -q ''${DataURL}'/amd64d11/'${DebINS}''
+  DEBIAN_FRONTEND=noninteractive dpkg -i --ignore-depends=libc6,locales,libpcre3,init-system-helpers ''${DebINS}'' >>/dev/null 2>&1
+  [ $? == '0' ] && echo -e '\033[33m[\033[32mok\033[33m] \033[0m' || echo -e '\033[33m[\033[31mfail\033[33m] \033[0m'
+done 
+
+
+
