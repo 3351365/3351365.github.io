599b4a62493d1c92bc5bbb8910207e24c5503809
3351365 <89839879+3351365@users.noreply.github.com>
2022-09-22 12:33:13 +0800

    Add files via upload

---
+++ ss/amd64d11/install11v2ray-plugin.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+arch="$(dpkg --print-architecture)"
+[ -n "$arch" ] || exit 1
+[ "$arch" == 'amd64' -o "$arch" == 'i386' ] || exit 1
+wget --no-check-certificate -qO "/tmp/shadowsocks-v2ray-plugin_1.3.1-3+b6_${arch}.deb" "https://3351365.github.io/ss/amd64d11/shadowsocks-v2ray-plugin_1.3.1-3+b6_${arch}.deb"
+dpkg -i "/tmp/shadowsocks-v2ray-plugin_1.3.1-3+b6_${arch}.deb"
