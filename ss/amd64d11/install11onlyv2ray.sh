e55941e38f531b46388df2dc97bce8b8f0ef0339
3351365 <89839879+3351365@users.noreply.github.com>
2022-11-28 20:20:46 +0800

    Create install11onlyv2ray.sh

---
+++ ss/amd64d11/install11onlyv2ray.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DataURL='https://3351365.github.io/ss'
+kBit=$(getconf LONG_BIT)
+[ "$kBit" == '32' ] && Bit='i386'
+[ "$kBit" == '64' ] && Bit='amd64'
+mkdir -p /tmp
+cd /tmp
+apt-get -q update
+apt-get install -y --no-install-recommends -f -qq apg asciidoc debhelper po-debconf intltool-debian libev-dev libpcre3-dev pkg-config xmlto libcap2-bin libpam-cap
+DebList="shadowsocks-v2ray-plugin_1.3.1-4_$Bit.deb"
+for DebINS in `echo $DebList`
+do
+  echo -ne '\033[33m'${DebINS}'\033[0m\t'
+  [ -f ''${DebINS}'' ] || wget --no-check-certificate -q ''${DataURL}'/amd64d11/'${DebINS}''
+  DEBIAN_FRONTEND=noninteractive dpkg -i --ignore-depends=libc6,locales,libpcre3,init-system-helpers ''${DebINS}'' >>/dev/null 2>&1
+  [ $? == '0' ] && echo -e '\033[33m[\033[32mok\033[33m] \033[0m' || echo -e '\033[33m[\033[31mfail\033[33m] \033[0m'
+done 
